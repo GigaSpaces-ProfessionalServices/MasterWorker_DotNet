8cb79664d3543a08497dc740f4ea5184b7c21600
rajivushah <rajiv@gigaspaces.com>
2016-06-06 10:23:11 -0400

    Add existing files

---
+++ Model/Base.cs
@@ -0,0 +1,23 @@
+﻿using GigaSpaces.Core.Metadata;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MasterWorkerModel
+{
+    [SpaceClass]
+    public class Base
+    {
+        [SpaceIndex(Type = SpaceIndexType.Basic)]
+        public string JobID { get; set; }
+
+        [SpaceID(AutoGenerate = false)]
+        public string TaskID { get; set; }
+        public string Payload { get; set; }
+        public string ServiceName { get; set; }
+        public string FunctionName { get; set; }
+        public Dictionary<String, String> Parameters { get; set; }
+    }
+}
---
+++ Model/BaseProcess.cs
@@ -0,0 +1,17 @@
+﻿using GigaSpaces.Core.Metadata;
+using System;
+
+namespace MasterWorkerModel
+{
+    [SpaceClass]
+    public class BaseProcess
+    {
+        [SpaceIndex(Type = SpaceIndexType.Basic)]
+        [SpaceID(AutoGenerate = false)]
+        public string ID { get; set; }
+        public string HostName { get; set; }
+        public int ProcessID { get; set; }
+        public DateTime StartDateTime { get; set; }
+        public DateTime LastUpdateDateTime { get; set; }
+    }
+}
